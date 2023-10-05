37828b43ff8d0cb0660032e996cae5d84f8b09dd
admin <admin@DESKTOP-B844U2B>
2023-10-05 13:22:58 +0530

    Window handling and Element Screenshot Updated

--- Automation/src/test/java/DemoQA/Automation/Testscript.java
+++ Automation/src/test/java/DemoQA/Automation/Testscript.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
@@ -16,8 +17,9 @@ import org.openqa.selenium.io.FileHandler;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
 
-
+import Pageobjects.BrowserWindowPage;
 import Pageobjects.ButtonPage;
 import Pageobjects.CheckBox;
 import Pageobjects.Elements;
@@ -46,11 +48,12 @@ public class Testscript {
 	}
 	@AfterTest
 	public void CloseBrowser(){
-		driver.close();
+		driver.quit();
 	}
 	
 	@Test(priority = 1)
 	public void Elements() {
+		//Handling Elements
 		Elements Elementspage = new Elements(); 
 		Elementspage.tabTextBox(driver).click();
 		Elementspage.txtFullName(driver).isDisplayed();
@@ -63,6 +66,7 @@ public class Testscript {
 	}
 	@Test(priority = 2)
 	public void Checkbox() {
+		//Handling CheckBox
 		CheckBox Chkboxpage = new CheckBox();
 		Chkboxpage.tabCheckbox(driver).click();
 		Chkboxpage.chkbxHome(driver).isDisplayed();
@@ -72,17 +76,19 @@ public class Testscript {
 	}
 	@Test(priority = 3)
 	public void RadioButtons() throws InterruptedException {
+		//Handling Radio buttons
 		RadioBtnPage radiobtnpage = new RadioBtnPage();
 		radiobtnpage.TabRadioBtn(driver).click();
 		radiobtnpage.RadioBtn(driver,"Yes").click();
 		String result = radiobtnpage.TextResult(driver).getText();
-		Reporter.log(result);
+		Reporter.log(result + "button is clicked");
 		radiobtnpage.RadioBtn(driver,"Impressive").click();
 		String result1 = radiobtnpage.TextResult(driver).getText();
-		Reporter.log("<br>"+result1);			
+		Reporter.log("<br>"+result1+" button is clicked");			
 	}
 	@Test(priority=4)
 	public void Buttons() throws IOException {
+		//Handling Buttons
 		ButtonPage Btnpage = new ButtonPage();
 		Btnpage.tabButton(driver).click();
 		action.doubleClick(Btnpage.DblClkbutton(driver)).perform();
@@ -94,18 +100,19 @@ public class Testscript {
 	}
 	@Test(priority=5)
 	public void Webtables() throws InterruptedException {
+		//Handling WebTables
 		driver.get("https://www.nyse.com/ipo-center/ipo-pricing-stats");
 		Thread.sleep(3000);
 		NysePageWT WebTable = new NysePageWT();
 		//ROW COUNT
 		int columncount = WebTable.ListCols(driver).size();
-		System.out.println("The total number of columns = " + columncount);
+		Reporter.log("The total number of columns = " + columncount);
 		//COLUMN COUNT
 		int rowcount = WebTable.ListRows(driver).size();
-		System.out.println("The total number of rows = " + rowcount);
+		Reporter.log("<br>The total number of rows = " + rowcount);
 		//Dynamic Value Changing Field
 		String FinanProceeds = WebTable.FinanceProceeds(driver).getText();
-		System.out.println("Technology Proceedings in $ = "+ FinanProceeds);
+		Reporter.log("<br>Technology Proceedings in $ = "+ FinanProceeds);
 		List<Integer> NumberList = new ArrayList<Integer>();
 		for(WebElement WElement : WebTable.NumberDeals(driver)) {
 			String numbers = WElement.getText();
@@ -113,10 +120,11 @@ public class Testscript {
 		}
 		int MinVal = Collections.min(NumberList);
 		String SectorName = WebTable.MinSector(driver, MinVal).getText();
-		Reporter.log("The Minium number of Deals is "+ MinVal + "and the Sector is " + SectorName);
+		Reporter.log("<br>The Minium number of Deals is "+ MinVal + " and the Sector is " + SectorName);
 	}
 	@Test (priority =6)
 	public void IFrames() throws InterruptedException {
+		//Handling Frames
 		driver.get("https://demo.automationtesting.in/Frames.html");
 		Thread.sleep(3000);
 		FramesPage FPage = new FramesPage();
@@ -135,4 +143,29 @@ public class Testscript {
 		Assert.assertTrue(VerifytxtBox2, "The Text Box should be interactable");
 		driver.switchTo().defaultContent();
 		}
+	@Test(priority=7)
+	public void WindowHandle () throws IOException {
+		driver.get("https://demoqa.com/browser-windows");
+		BrowserWindowPage window = new BrowserWindowPage();
+		String ParentHandle = driver.getWindowHandle();
+		window.btn_NewTab(driver).click();
+		Set <String> navWindow1 = driver.getWindowHandles();
+		for(String iterator : navWindow1 ) { 
+			driver.switchTo().window(iterator);
+		}
+		String label1 = window.lbl_text(driver).getText();
+		Reporter.log( "<br>"+label1+ "Window is switched and Driver can locate the text");
+		driver.switchTo().window(ParentHandle);
+		window.btn_NewWin(driver).click();
+		Set <String> navWindow2 = driver.getWindowHandles();
+		for(String iterator : navWindow2 ) { 
+			driver.switchTo().window(iterator);
+		}
+		String label2 = window.lbl_text(driver).getText();
+		File lbl1 = window.lbl_text(driver).getScreenshotAs(OutputType.FILE);
+		File slbl1 = new File("./Screenshots/"+label2+".png");
+		FileHandler.copy(lbl1, slbl1);
+		Reporter.log("<br>"+ label2+ "Window is switched and Driver can locate the text");
+
+	}	
 }
